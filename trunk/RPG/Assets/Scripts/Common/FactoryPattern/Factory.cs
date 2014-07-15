5cd30dd907e6bdb36b817025dc40b68f9e90bd0f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2014-07-15 22:27:01 +0900

    ファクトリーパターン実装

---
+++ trunk/RPG/Assets/Scripts/Common/FactoryPattern/Factory.cs
@@ -0,0 +1,38 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : Factory.cs
+*
+*     * Description : ファクトリーパターン.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPG.Common
+{
+	/// <summary>
+	/// ファクトリーパターン.
+	/// </summary>
+	public class Factory<T> where T : FactoryElement
+	{
+		private List<T> productList;
+
+		public Factory()
+		{
+			this.productList = new List<T>();
+		}
+
+		public void Add( T product )
+		{
+			this.productList.Add( product );
+		}
+
+		public T Clone( int id )
+		{
+			return productList.Find( p => p.Id == id ).Clone() as T;
+		}
+	}
+}
---
+++ trunk/RPG/Assets/Scripts/Common/FactoryPattern/FactoryElement.cs
@@ -0,0 +1,30 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : FactoryElement.cs
+*
+*     * Description : ファクトリー要素.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPG.Common
+{
+	/// <summary>
+	/// ファクトリー要素.
+	/// </summary>
+	public abstract class FactoryElement
+	{
+		public int Id{ private set; get; }
+
+		public FactoryElement( int id )
+		{
+			this.Id = id;
+		}
+
+		public abstract FactoryElement Clone();
+	}
+}
