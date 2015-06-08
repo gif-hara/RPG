e5a62fb20565df7b0e6bf37f4330f125c00688b7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-06-09 01:56:12 +0900

    バトル部分の改修中

---
+++ trunk/RPG/Assets/Scripts/Battle/System/FactoryPattern/AttackCommandData.cs
@@ -0,0 +1,70 @@
+﻿/*===========================================================================*/
+/*
+*     * FileName    : AttackCommandData.cs
+*
+*     * Description : 戦うコマンドデータ.
+*
+*     * Author      : Hiroki_Kitahara.
+*/
+/*===========================================================================*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Common;
+
+namespace RPG.Battle
+{
+	/// <summary>
+	/// 戦うコマンドデータ.
+	/// </summary>
+	public class AttackCommandData : CommandData
+	{
+//		/// <summary>
+//		/// 攻撃回数.
+//		/// </summary>
+//		private int attackNumber = 0;
+//
+//		public AttackCommandData()
+//			:base( BattleTypeConstants.CommandType.Attack )
+//		{
+//
+//		}
+//
+//		public override FactoryElement Clone ()
+//		{
+//			InternalInitialize();
+//			return this;
+//		}
+//
+//		#region implemented abstract members of CommandData
+//
+//		protected override Queue<List<CommandEventBase>> Notice (BattleMemberData executeMember, CommandEventHolder eventHolder)
+//		{
+//			this.attackNumber = 1;
+//			var result = new Queue<List<CommandEventBase>>();
+//			var infoList = new List<CommandEventBase>();
+//			infoList.Add( eventHolder.Get( CommandEventConstants.EventName.Information_AllyAttack ) );
+//			result.Enqueue( infoList );
+//			return result;
+//		}
+//
+//		protected override Queue<List<CommandEventBase>> Execute (BattleMemberData executeMember, CommandEventHolder eventHolder)
+//		{
+//			return null;
+//			
+//		}
+//
+//		protected override Queue<List<CommandEventBase>> Result (BattleMemberData executeMember, CommandEventHolder eventHolder)
+//		{
+//			return null;
+//		}
+//
+//		protected override Queue<List<CommandEventBase>> End (BattleMemberData executeMember, CommandEventHolder eventHolder)
+//		{
+//			return null;
+//		}
+//
+//		#endregion
+//
+	}
+}
