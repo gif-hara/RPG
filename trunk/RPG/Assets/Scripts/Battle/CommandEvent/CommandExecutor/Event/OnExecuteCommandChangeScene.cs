1f6564131ef05a2efba8bb9de1b6ff5391e57ceb
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-06-28 03:44:16 +0900

    勝利処理の実装

---
+++ trunk/RPG/Assets/Scripts/Battle/CommandEvent/CommandExecutor/Event/OnExecuteCommandChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace RPG.Battle
+{
+	/// <summary>
+	/// コマンド実行イベント時にシーンを切り替えるコンポーネント.
+	/// </summary>
+	public class OnExecuteCommandChangeScene : MyMonoBehaviour
+	{
+		[SerializeField]
+		private string sceneName;
+
+		[Attribute.MessageMethodReceiver( BattleMessageConstants.ExecuteCommandMessage )]
+		void OnExecuteCommand( BattleMessageConstants.ExecuteCommandHook hook )
+		{
+			Application.LoadLevel( sceneName );
+		}
+	}
+}
---
+++ trunk/RPG/Assets/Scripts/Battle/System/StateElement/BattleState/OnActiveStateStartCommand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using RPG.Common;
+using RPG.Framework;
+
+namespace RPG.Battle
+{
+	/// <summary>
+	/// ステートがアクティブになった際にコマンドを実行するコンポーネント.
+	/// </summary>
+	public class OnActiveStateStartCommand : MyMonoBehaviour
+	{
+		[SerializeField]
+		private CommandExecuter refExecuter;
+
+		[SerializeField]
+		private BattleTypeConstants.CommandType commandType;
+
+		[SerializeField]
+		private CommandEventMediator prefabMediator;
+
+		[Attribute.MessageMethodReceiver( BattleMessageConstants.ActiveStateMessage )]
+		void OnActiveState()
+		{
+			var mediator = Instantiate( this.prefabMediator );
+			mediator.SetEvent( refExecuter );
+			refExecuter.SetCommandType( this.commandType );
+			refExecuter.StartCommand();
+		}
+	}
+}
