64cd0a8026851722b1278384a0c693b82514ad02
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2015-07-07 23:58:48 +0900

    弱っているキャラクターを優先して術を実行出来るよう実装

---
+++ trunk/RPG/Assets/Scripts/Battle/CommandEvent/CommandExecutor/Event/OnExecuteCommandSetTargetAtWeakCharacter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using RPG;
+
+namespace RPG.Battle
+{
+	/// <summary>
+	/// コマンド実行イベント時に弱っているキャラクターを優先して選択するコンポーネント.
+	/// </summary>
+	public class OnExecuteCommandSetTargetAtWeakCharacter : MyMonoBehaviour, I_OnExecuteCommandHookable
+	{
+		[Attribute.MessageMethodReceiver( Battle.MessageConstants.ExecuteCommandMessage )]
+		public void OnExecuteCommand (Battle.MessageConstants.ExecuteCommandHook hook)
+		{
+			var selectCommandData = AllPartyManager.Instance.ActiveTimeMaxBattleCharacter.SelectCommandData;
+			selectCommandData.SetTarget( selectCommandData.GetTargetGroupSafe().WeakCharacter );
+		}
+	}
+}
